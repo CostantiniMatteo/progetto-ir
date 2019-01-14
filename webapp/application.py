7f26034d75d4ef4d2930f7646a37940300276ad9
Dario <bahnhof2000@yahoo.it>
2019-01-14 17:52:05 +0100

    webapp

--- webapp/application.py
+++ webapp/application.py
@@ -19,16 +19,6 @@ def home():
     if 'q' in request.args:
         r = requests.get(BASE_URL + "search", params={'q': request.args.get('q')})
         r_json = r.json()
-        # tweetIds = [d['tweetId'] for d in r_json]
-        # tweet_html = []
-        # for tweetId in tweetIds:
-        #     try:
-        #         r_tweet = requests.get(EMBED_URL + tweetId).json()
-        #         tweet_html += [r_tweet['html']]
-        #     except:
-        #         pass
-
-        # return render_template('index.html', tweet_html=tweet_html)
         return jsonify(r_json)
 
     else:
@@ -40,7 +30,11 @@ def get_tweet_html():
     t_id = request.args.get("tweetId")
     print(t_id)
     r = requests.get(EMBED_URL + str(t_id))
-    print(r.json())
+
+    if r.status_code != 200:
+        print(t_id)
+        return ('', 204)
+
     return jsonify(r.json())
 
 
