09f24641cf8bedd1c7b7c034742cc9b63098b351
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2019-01-15 14:52:26 +0100

    boh

--- tweet-tweet-go/src/main/java/cgp/ttg/engine/App.java
+++ tweet-tweet-go/src/main/java/cgp/ttg/engine/App.java
@@ -11,18 +11,26 @@ public class App {
     public static void main(String[] args) throws Exception {
         var in = new Scanner(System.in);
 //        doIndexing();
-        doQuery(in, null);
+        doQuery(in, UserProfile.getProfile("user1"));
     }
 
     private static void doQuery(Scanner in, UserProfile u) {
+        var topic = "";
         while (true) {
             System.out.println("Escimi la query...");
             var query = in.nextLine();
-            if (!"".equals(query)) {
-                QueryEngine.match(query, 150, false, true, true, null, null, null, null);
-                System.out.println("\n\n");
-            } else {
+
+            if ("".equals(query)) {
                 break;
+            } else if (query.startsWith("topic")) {
+                topic = query.split(" ")[1];
+            } else {
+                if ("none".equals(topic)) {
+                    QueryEngine.match(query, 150, false, true, true, null, null, null, null);
+                } else {
+                    QueryEngine.match(query, 150, false, true, true, null, null, topic, u);
+                }
+                System.out.println("\n\n");
             }
         }
     }
