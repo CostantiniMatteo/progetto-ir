f3e455ae53b3ddd144a36c78d5b27b0f6f5e920e
Getrio <bahnhof2000@yahoo.it>
2019-01-18 23:11:06 +0100

    aggiunti docker file forse

--- webapp/application.py
+++ webapp/application.py
@@ -1,18 +1,14 @@
 from flask import Flask, render_template, request, abort, make_response, redirect, url_for, jsonify
-from flask_cors import *
 import requests
 import json
 
 
 app = Flask(__name__, static_url_path='')
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 BASE_URL = "http://localhost:8080/"
 
 @app.route('/', methods = ['GET', 'POST'])
-@cross_origin()
 def home():
 
     if 'q' in request.args:
