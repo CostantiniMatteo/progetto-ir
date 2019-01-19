c5392c8f27197a3c11de5ce0927794a4cc5aae9d
Getrio <bahnhof2000@yahoo.it>
2019-01-19 13:50:56 +0100

    dockerizzazione

--- tweet-tweet-go/src/main/java/cgp/ttg/engine/UserProfile.java
+++ tweet-tweet-go/src/main/java/cgp/ttg/engine/UserProfile.java
@@ -13,7 +13,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class UserProfile {
-    private static final String USER_PROFILES_PATH = "tweet-tweet-go/src/main/resources/";
+    private static final String USER_PROFILES_PATH = "/app/users/";
     private static List<String> userIds = Arrays.asList("user1", "user2", "user3", "user4", "user5");
     private static List<String> topics = Arrays.asList("sport", "music", "tech", "cs", "politics", "cinema", "food", "science", "cars", "finance");
     private static HashMap<String, HashMap<String, List<String>>> userDocumentsByTopic = initUserDocumentsByTopic();
--- tweet-tweet-go/tweet-tweet-go.Dockerfile
+++ tweet-tweet-go/tweet-tweet-go.Dockerfile
@@ -10,7 +10,8 @@ RUN mvn verify clean --fail-never
 ADD . /usr/src/app
 RUN mvn install -DskipTests
 
-FROM openjdk:8-jre-alpine
-COPY --from=build /usr/src/app/target/tweet-tweet-go-0.0.1-SNAPSHOT.jar /app/app.jar
+FROM adoptopenjdk/openjdk11:jdk11u-alpine-nightly-slim
+COPY --from=build /usr/src/app/target/progetto-ir-0.0.1-SNAPSHOT.jar /app/app.jar
+COPY --from=build /usr/src/app/src/main/resources /app/users
 EXPOSE 8080
-ENTRYPOINT ["/usr/bin/java", "-jar", "/app/app.jar"]
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
