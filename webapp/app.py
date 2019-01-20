f3316216dc4ffda6656f99045bc027b0088e2041
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2019-01-20 10:38:13 +0100

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,8 @@ import json
 app = Flask(__name__, static_url_path='')
 
 
-BASE_URL = "http://localhost:8080/"
+# BASE_URL = "http://localhost:8080/"
+BASE_URL = "http://tweet-tweet-go:8080/"
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
