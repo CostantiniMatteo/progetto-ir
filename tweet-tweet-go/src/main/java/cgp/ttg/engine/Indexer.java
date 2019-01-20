114c0cdc22f18ed5f7af8047d57a00d0a2f423cb
Dario <bahnhof2000@yahoo.it>
2019-01-20 10:10:05 +0100

    webapp

--- tweet-tweet-go/src/main/java/cgp/ttg/engine/Indexer.java
+++ tweet-tweet-go/src/main/java/cgp/ttg/engine/Indexer.java
@@ -15,7 +15,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Indexer {
-    public static final String INDEX_PATH = "index";
+    //public static final String INDEX_PATH = "index";
+    //public static final String USER_INDEX_PATH = "user_index";
+    public static final String INDEX_PATH = "/app/index/";
     public static final String USER_INDEX_PATH = "user_index";
     public static final FieldType tweetField = new FieldType() {{
         setTokenized(true);
